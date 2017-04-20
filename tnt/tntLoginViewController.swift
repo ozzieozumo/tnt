fdb3c82fdc781ae6995001d805a53037dec6b390
ozzieozumo <luke.d.everett@gmail.com>
2017-04-20 18:17:02 -0400

    add prints to verify FB profile

--- tnt/tntLoginViewController.swift
+++ tnt/tntLoginViewController.swift
@@ -14,9 +14,11 @@ import AWSCognito
 class tntLoginViewController: UIViewController {
     
     var fbToken: FBSDKAccessToken?
-    var credentialsProvider: AWSCredentialsProvider?
+    var credentialsProvider: AWSCognitoCredentialsProvider?
     var fbLoginManager: FBSDKLoginManager?
     
+    
+    @IBOutlet weak var btnDynamo: UIButton!
     
     override func viewDidLoad() {
         
@@ -36,7 +38,7 @@ class tntLoginViewController: UIViewController {
             if self.fbLoginManager == nil {
                 self.fbLoginManager = FBSDKLoginManager()
                 
-                self.fbLoginManager?.logIn(withReadPermissions: nil, from: self) {
+                self.fbLoginManager?.logIn(withReadPermissions: ["public_profile"], from: self) {
                     (result, error) -> Void in
                     
                     if (error != nil) {
@@ -47,6 +49,8 @@ class tntLoginViewController: UIViewController {
                         }
                     } else if result!.isCancelled {
                         //Do nothing
+                        print("FB Login result was cancelled")
+                    
                     } else {
                         // Now the current access token should be set on Facebook
                         self.fbToken = FBSDKAccessToken.current()
@@ -66,8 +70,23 @@ class tntLoginViewController: UIViewController {
     func CognitoLogin() {
      
         
-        // assert(self.fbToken != nil)
+        // assert(self.fbToken != nil)  also, this won't be on the main thread
         
+        // print out some information about the Facebook Token and User Profile
+        
+        print("tnt: FBSDK Version \(FBSDKSettings.sdkVersion())")
+        
+        FBSDKProfile.loadCurrentProfile  { (profile, error) -> Void in
+            // Completion handler for loadCurrentProfile
+            if (error != nil) {
+                    print("tnt:error in loadCurrentProfile")
+                }
+            else {
+                let fbProfile = FBSDKProfile.current()
+                print("tnt: FBSDK User Profile \(fbProfile!.name)")
+            }
+        }
+    
         let fblogin = [Constants.FACEBOOK_PROVIDER : fbToken!.tokenString!]
         
         if self.credentialsProvider == nil {
@@ -83,13 +102,29 @@ class tntLoginViewController: UIViewController {
             
             AWSServiceManager.default().defaultServiceConfiguration = configuration
             
-            print("Logged in to AWS Cognito using FB login")
+            print("tnt: Logged in to AWS Cognito")
+            print("tnt: \(configuration?.userAgent)")
+            
+            // could call getIdentityId here and print out in continuation block
+            
+            self.credentialsProvider?.getIdentityId().continueWith { task in
+                
+                print("tnt: Cognito Identity ID \(self.credentialsProvider?.identityId)")
+                
+            }
+            
         } else {
+            // we already have a credentials provider 
             // should probably call getIdentityId or something here
         }
-
         
+        DispatchQueue.main.async {
+            
+            self.btnDynamo.isUserInteractionEnabled = true
+        }
     }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
